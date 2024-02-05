3b4f59dadb757d3799f99a34b32360b03814ae90
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-05 20:09:59 +0000

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./routes/error-page";
+import Contact from "./routes/contact";
+import Projects from "./routes/projects";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contact/",
+    element: <Contact />,
+  },
+  {
+    path: "projects/",
+    element: <Projects />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
