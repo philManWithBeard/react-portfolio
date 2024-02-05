73167fc96b7aef758a22b9aac62675136e270c70
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-05 20:10:53 +0000

    contact page

---
+++ src/routes/contact.jsx
@@ -0,0 +1,10 @@
+import { Form } from "react-router-dom";
+import Layout from "../components/layout";
+
+export default function Contact() {
+  return (
+    <>
+      <Layout></Layout>
+    </>
+  );
+}
