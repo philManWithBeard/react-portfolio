5b90d52717c27f44f1b0a031ab5449e399b29eaf
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-09 15:29:48 +0000

    created contact page

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,10 +1,55 @@
 import { Form } from "react-router-dom";
 import Layout from "../components/layout";
+import ContactForm from "../components/contactForm";
+import { Link } from "react-router-dom";
+import PhilsCV from "../assets/PhilsCV.pdf";
 
 export default function Contact() {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <div className="container-fluid about-me" id="about-me">
+          <div className="container">
+            <div className="row align-items-start justify-content-center">
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <h2 className="sectionHeading">Contact Me</h2>
+                <ContactForm />
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12 row align-items-start justify-content-center px-5">
+                <p className="">phil@fullystacked.com</p>
+                <Link
+                  to="mailto:phil@fullystacked.io"
+                  target="_blank"
+                  className="btn-dark d-inline-flex align-items-center btn btn-primary btn-lg px-4 d-block py-1 my-3"
+                >
+                  Email
+                </Link>
+                <Link
+                  to={PhilsCV}
+                  target="_blank"
+                  className="btn-dark d-inline-flex align-items-center btn btn-primary btn-lg px-4 d-block py-1 my-3"
+                >
+                  My CV
+                </Link>
+                <Link
+                  to="https://github.com/philManWithBeard/"
+                  target="_blank"
+                  className="btn-dark d-inline-flex align-items-center btn btn-primary btn-lg px-4 d-block py-1  my-3"
+                >
+                  My Github
+                </Link>
+                <Link
+                  to="https://www.linkedin.com/in/phil-burrows-03b2432b4/"
+                  target="_blank"
+                  className="btn-dark d-inline-flex align-items-center btn btn-primary btn-lg px-4 d-block py-1  my-3"
+                >
+                  LinkedIn
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
     </>
   );
 }
---
+++ src/routes/project.jsx
@@ -0,0 +1,51 @@
+import Layout from "../components/layout";
+import { useParams } from "react-router-dom";
+import ProjectsObj from "../assets/projects.json";
+import ProjectCard from "../components/projectCard";
+
+export default function Project() {
+  const { projectId } = useParams();
+  const project = ProjectsObj.filter(
+    (project) => project.slug === projectId
+  )[0];
+  return (
+    <>
+      <Layout>
+        <main>
+          <div className="py-5 container-fluid">
+            <div className="container ">
+              <div className="row justify-content-center">
+                <div className="col-lg-4 col-md-3 col-sm-4 col-3">
+                  <img
+                    src={project.imageSrc}
+                    className="card-img-top card border-dark"
+                    alt={project.imageAlt}
+                  />
+                </div>
+                <div className="col-xl-5 col-lg-4 col-md-6 col-sm-6 col-8">
+                  <h2 className="display-5 py-1">{project.name}</h2>
+                  <p className="py-1">{project.skills}</p>
+                  <p className="py-1">{project.description}</p>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="btn-dark d-inline-flex align-items-center btn btn-primary btn-lg px-4 d-block py-1"
+                  >
+                    Visit repo
+                  </a>
+                  <a
+                    href={project.deployedApp}
+                    target="_blank"
+                    className="btn-dark d-inline-flex align-items-center mx-3 btn btn-primary btn-lg px-4 d-block py-1"
+                  >
+                    Visit site
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  );
+}
