4bbdd36add5007a990d2fa93d61b327cd8170af6
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-09 15:29:03 +0000

    changed class to classname

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,10 +4,10 @@ import { HashLink as Link } from "react-router-hash-link";
 export default function Header() {
   return (
     <>
-      <header class="navbar navbar-expand-lg sticky-top p-4">
-        <div class="container-fluid">
+      <header className="navbar navbar-expand-lg sticky-top p-4">
+        <div className="container-fluid">
           <Link to={"/#"}>
-            <span class="navbar-brand h1 m-0">Phil Burrows</span>
+            <span className="navbar-brand h1 m-0">Phil Burrows</span>
           </Link>
           <button
             className="navbar-toggler"
@@ -18,27 +18,27 @@ export default function Header() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <nav class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <Link to={"/#about-me"} class="nav-link">
+          <nav className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to={"/#about-me"} className="nav-link">
                   About Me
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link to={"/#skills"} class="nav-link">
+              <li className="nav-item">
+                <Link to={"/#skills"} className="nav-link">
                   Skills
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link to={"projects/"} class="nav-link">
+              <li className="nav-item">
+                <Link to={"/projects"} className="nav-link">
                   Projects
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link to={"/contact"} class="nav-link">
+              <li className="nav-item">
+                <Link to={"/contact"} className="nav-link">
                   Contact
                 </Link>
               </li>
