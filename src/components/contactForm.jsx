deab44c58b20b78d836e8a8bc6babab0fa7c3a65
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-09 15:28:41 +0000

    created contact form component

---
+++ src/components/contactForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
+  const sendEmail = (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setStateMessage("Message sent!");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        },
+        (error) => {
+          setStateMessage("Something went wrong, please try again later");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 5000); // hide message after 5 seconds
+        }
+      );
+
+    // Clears the form after sending the email
+    e.target.reset();
+  };
+  return (
+    <form onSubmit={sendEmail} className="align-items-start row g-2">
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input
+        type="submit"
+        value="Let's Talk"
+        disabled={isSubmitting}
+        className="btn-dark d-inline-flex align-items-center btn-dark d-inline-flex align-items-center btn btn-primary btn-lg px-4 my-4 btn-lg px-4"
+      />
+      {stateMessage && <p>{stateMessage}</p>}
+    </form>
+  );
+};
+export default ContactForm;
