2c67dbe9e390f2c8c6e31b535080d7468647ddb5
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-02-11 21:28:45 +0000

    Ensure all project boxes are the same height

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -5,22 +5,28 @@ export default function ProjectCard(props) {
   return (
     <>
       <div className="cardContainer col-lg-4 col-md-12 col-sm-12">
-        <div className="card border-dark">
+        <div className="card border-dark h-100">
           <img
             src={props.imageSrc}
             className="card-img-top"
             alt={props.imageAlt}
           />
-          <div className="card-body">
-            <h5 className="card-title">{props.name}</h5>
-            <p className="card-text">{props.description}</p>
-            <Link
-              to={"/projects/" + props.slug}
-              href={"https://philmanwithbeard.github.io/projects/" + props.slug}
-              className="btn-dark rounded-0 d-inline-flex align-items-center btn btn-primary btn-lg px-4"
-            >
-              See more
-            </Link>
+          <div className="card-body d-flex flex-column justify-content-between">
+            <div className="my-2">
+              <h5 className="card-title">{props.name}</h5>
+              <p className="card-text">{props.description}</p>
+            </div>
+            <div>
+              <Link
+                to={"/projects/" + props.slug}
+                href={
+                  "https://philmanwithbeard.github.io/projects/" + props.slug
+                }
+                className="btn-dark rounded-0 d-inline-flex btn btn-primary btn-lg px-4"
+              >
+                See more
+              </Link>
+            </div>
           </div>
         </div>
       </div>
